28e3e9d779b1fc2a1c101c420c3da5941bcc1531
kiki <772765729@qq.com>
2020-04-08 10:09:09 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
 import React from 'react';
 import styled from 'styled-components'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+function Home() {
+  return <h2>Home</h2>;
+}
 
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function NoMatch() {
+  return <h2>404</h2>
+}
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Redirect exact from='/' to='/'></Redirect>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
